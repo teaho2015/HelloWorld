8b8459a35c6cb0e397f855e5ce0ec805f68c4eee
TeaHo <tea2015@qq.com>
2016-03-04 00:32:19 +0800

    Initial commit

---
+++ src/main/java/com/tea/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by 庭亮 on 2016/3/3.
+ */
+package com.tea.hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+
+}
